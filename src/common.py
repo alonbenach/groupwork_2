f392321e646831fc94efb381b9eab0ee8f12fa72
alonbenach <benach@gmail.com>
2023-05-23 19:52:13 +0200

    common module expanded, code finished w/ comments

--- src/common.py
+++ src/common.py
@@ -3,11 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+import statsmodels
 import statsmodels.tsa.api as tsa
+import statsmodels.api as sm
 
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
 
 
 def Stationarity_analysis(y, yDesc):
@@ -62,3 +64,108 @@ def Stationarity_analysis(y, yDesc):
     print(
         "===================================================================================================="
     )
+
+
+def test_het(results):
+    """
+    Testing for heteroskedasticity
+
+    :param results: Univariate time series (panda series).
+
+    :print: Goldfeld-Quandt test stat and p-value (null hypothesis - no heteroskedasticity)
+    """
+    test_results = results.test_heteroskedasticity(method="breakvar")
+    test_results = test_results[0]
+    printout = print(
+        f"""
+        -------------------------------------------------------------------
+        Testing for heteroskedasticity in time series: Goldfeld-Quandt test
+            H0: no heteroskedasticity
+            Goldfeld-Quandt test stat:{test_results[0]}
+                              p-value:{test_results[1]}
+        -------------------------------------------------------------------
+        """
+    )
+
+    return printout
+
+
+def test_norm(results):
+    """
+    Testing for normality of errors - Jarque Bera test:
+
+    :param results: Univariate time series (panda series).
+
+    :print: Jarque Bera test test stat and p-value (null hypothesis - normal destribution)
+    """
+    test_results = results.test_normality(method="jarquebera")
+    test_results = test_results[0]
+    printout = print(
+        f"""
+        -------------------------------------------------------------------
+        Testing normality - Jarque Bera test:
+            H0: normal destribution
+            Jarque Bera test stat:{test_results[0]}
+                              p-value:{test_results[1]}
+        -------------------------------------------------------------------
+        """
+    )
+
+    return printout
+
+
+def serial_corr(results, lagnum):
+    """
+    Testing correlation for lags in ARMA model results
+
+    :param results: model results of ARMA model (statsmodels.tsa.arima.model).
+
+    :lagnum: int, number of lags to be used for individual correlation test
+    """
+    BPres = results.test_serial_correlation("ljungbox", lags=lagnum)
+    BPres = BPres[0]
+
+    BPstat = pd.DataFrame(
+        {
+            "lag": list(range(1, lagnum + 1)),
+            "ljungbox stat": BPres[0],
+            "p-value": BPres[1],
+        }
+    )
+    printout = print(
+        f"""
+    ----------------------------------------------------------------------------------------------
+    Testing serial correlation - Ljung-Box Q test:
+                H0: no serial correlation for a given lag
+    {BPstat}
+    ----------------------------------------------------------------------------------------------
+    """
+    )
+    return printout
+
+
+def serial_corr_all_lags(results, lagnum):
+    """
+    Testing serial correlation for all lags up to certain lag together in ARMA model results
+
+    :param results: model results of ARMA model (statsmodels.tsa.arima.model).
+
+    :lagnum: int, number of lags to be used for individual correlation test
+    """
+    df = sm.stats.acorr_ljungbox(
+        results.resid, lags=[lagnum], return_df=False, model_df=4, boxpierce=True
+    )
+
+    printout = print(
+        f"""
+    ----------------------------------------------------------------------------------------------
+    Testing serial correlation - Ljung-Box Q test:
+                H0: no serial correlation all lags up to lag {lagnum} together
+
+            Ljungbox test stat: {df[0][0]}
+            p-value: {df[1][0]}
+
+    ----------------------------------------------------------------------------------------------
+    """
+    )
+    return printout
